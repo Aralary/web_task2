7db504e1adc775543fd7ffc22223412d7664fb01
aralary <makarov.0205@mail.ru>
2022-04-13 17:00:09 +0300

    add paginator

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from random import random
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,13 +11,13 @@ QUESTIONS = [
         "tag2": f"tag {i - 1}",
         "tag3": f"tag {i * i}",
         "id": i
-    } for i in range(0, 5)
+    } for i in range(0, 10)
 ]
 
 ANSWERS = [
     {
         "title": f"Answer {i}"
-    } for i in range(1, 3)
+    } for i in range(0, 10)
 ]
 
 LOOK_QUESTIONS = [
@@ -32,7 +32,12 @@ LOOK_QUESTIONS = [
 
 
 def index(request):
-    return render(request, "index.html", {"questions": QUESTIONS})
+    question_list = QUESTIONS
+    paginator = Paginator(question_list, 3)
+
+    page = request.GET.get('page')
+    questions = paginator.get_page(page)
+    return render(request, "index.html", {"questions": questions})
 
 
 def signup(request):
@@ -42,10 +47,15 @@ def signup(request):
 def ask(request):
     return render(request, "ask.html")
 
+
 def question(request, ix: int):
-    return render(request, "question.html", {"question": QUESTIONS[ix], "answers": ANSWERS})
+    answer_list = ANSWERS
+    paginator = Paginator(answer_list, 2)
+
+    page = request.GET.get('page')
+    answers = paginator.get_page(page)
+    return render(request, "question.html", {"question": QUESTIONS[ix], "answers": answers})
 
 
 def login(request):
     return render(request, "login.html")
-
